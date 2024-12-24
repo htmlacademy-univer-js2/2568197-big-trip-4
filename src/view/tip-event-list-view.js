76b474dfbf9a2eeeaa51beb0d32beec76c4aef4d
Nikita Bystritsky <nbystritsky@gmail.com>
2024-12-24 13:12:02 +0500

    feat: 1.8. Время разбивать камни done

--- src/view/tip-event-list-view.js
+++ src/view/tip-event-list-view.js
@@ -1,5 +1,4 @@
 import {createElement} from '../render';
-
 const createTipEventListViewTemplate = () => '<ul class="trip-events__list"></ul>';
 export default class TipEventList {
   getTemplate() {
